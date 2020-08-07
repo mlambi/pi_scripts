d3a6d89158119cc3bc16d0635f89099ff1e41b93
mlambi <mlambi@users.noreply.github.com>
2020-08-07 18:29:57 -0400

    adding update_hosts

---
+++ update_hosts.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+# an attempt at writing a script to update the hosts file
+# this does the job.  you need to start from a default
+# hosts file.  This will append shared pi_scripts/hosts.txt
+# 
+
+while IFS= read -r line;do
+    echo "$line" >> /etc/hosts
+done < "/home/pi/Desktop/pi_scripts/hosts.txt"
+echo "completed"
