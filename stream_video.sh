1c944d387e1c9d78bd2e017223ace817a1844d9e
mlambi <mlambi@users.noreply.github.com>
2021-03-28 12:21:27 -0400

    Fixed a typo in the video script.

--- stream_video.sh
+++ stream_video.sh
@@ -4,4 +4,4 @@
 # to receive:
 # mplayer -x 1280 -y 720 -geometry 0:0 -fps 200 -demuxer h264es -noborder ffmpeg://tcp://x.x.x.x:5000
 
-raspivid -b 4000000 -t 0 -o - | mc -l -p 5000
+raspivid -b 4000000 -t 0 -o - | nc -l -p 5000
