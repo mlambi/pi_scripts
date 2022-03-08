d3f28cd7ea6673f0a8eceeb46cefb9358bd15b75
mlambi <mlambi@users.noreply.github.com>
2022-03-08 06:53:33 -0500

    Added who-is-alive script.

--- mqtt_who-is-alive.py
+++ mqtt_who-is-alive.py
@@ -14,18 +14,18 @@ import logging
 # need to learn why other forms of importind datetime did not work
 from datetime import datetime, timedelta
 
-# logging.basicConfig(filename='who_is_alive.log', format='%(levelname)s:%(message)s', level=logging.DEBUG)
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+# logging.basicConfig(filename='who_is_alive.log', format='%(levelname)s:%(message)s', level=logging.INFO)
+# when working things out I found it easier to NOT log to a file
+# the file is good for debugging the service; it can grow fast
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 uh.set_layout(uh.PHAT)
 uh.brightness(0.4)
 
-# initialize a list for host data
-# experimenting with different methods to initialize a list
-# hosts_list = list()
-# making it global!
+# initialize a list for host data, and making it GLOBAL!
+# There are lots of bad vibes about globals, but it makes things work here.
 global hosts_list
-hosts_list = ['host1', 'host2', 'host3', 'host4', 'host5', 'host6', 'host7', 'host8']
+hosts_list = [{'hostid':'host0'}, {'hostid':'host1'}, {'hostid':'host2'}, {'hostid':'host3'}, {'hostid':'host4'}, {'hostid':'host5'}, {'hostid':'host6'}, {'hostid':'host7'}]
 logging.info("Hosts_list created")
 for item in hosts_list:
     logging.debug(item)
@@ -35,74 +35,78 @@ def on_connect(client, userdata, flags, rc):
     logging.info("OnConnect:  Connected with result code {0}".format(str(rc)))
     client.subscribe("IamAlive")
 
-
+# OnMessage passes the IamAlive data to other functions
 def on_message(client, userdata, msg):
-    logging.debug("OnMessage:  Message received-> " + msg.topic + " " + str(msg.payload))
     values =  json.loads(msg.payload)
-    logging.debug("OnMessage:  The converted dictionary is : " + str(values))
-    logging.debug(type(values))
-    hostid = (values["id"])
-    hostid_type = type(hostid)
-    logging.debug("OnMessage:  The hostid is: %s and the type is %s",  hostid, hostid_type)
-    logging.debug("onMessage:  The timestamp is:  %s", values["timestamp"])
-    # this is where the msg is assigned to the list
-    # the old timestamp needs to be read before now
-    update_hosts_list(hostid, values)
-    hosts_list[hostid]  = values
-    # how to replace the above and put it in the above above
-    logging.debug("OnMessage: Print hosts_list: %s", hosts_list)
-    host_data = (values["timestamp"], values["id"], values["hostname"], values["cpu_temp"])
-    set_color(host_data)
+    hostid = int(values["id"])
+    logging.debug("OnMessage:  hostid:  %s, message:  %s", hostid, values)
+    update_hosts(hostid, values)
 
-# this isnt finished...
-# and it doesn't need anything passed in to it
-# it just needs to run periodically and loop through the hosts_list to find stale hosts
-# and then reset that index to the generic 'hostx' format.
-# the logic is to add time to the timestamp we want to monitor
-# if 
-def update_hosts_list(hostid, values):
-    print()
+def update_health():
     new_timestamp = datetime.now()
-    logging.info('New timestamp is: %s ', new_timestamp)
     for i in range(8):
         try:
-            old_timestamp = datetime.strptime((hosts_list[i]["timestamp"]), "%Y%m%d %H:%M:%S")
-            logging.info("HostIndex: %s:  old_timestamp:  %s", i, old_timestamp)
-            time_diff = old_timestamp + timedelta(minutes = 1)
-            if time_diff < new_timestamp:
-                logging.info("time_diff + 1m is less than new_timestamp, Update hosts_list")
-                hosts_list[i] = "host" + str(i)
-                for x in range(4):
-                    logging.info("setting pixel %s : %s", i, x)
-                    uh.set_pixel(i,x,0,0,0)
-                logging.info("all pixels set")
-                uh.show()
+            old_timestamp = datetime.strptime((hosts_list[i]["timestamp"]), "Y%m%d %H:%M:%S")
+            # If there is not an old_timestamp _except_
+            logging.debug("UpdateHosts:  id:  %s, old_timestamp:  %s, new_timestamp:  %s", 
+                    i, old_timestamp, new_timestamp)
+            
+            # Over 5 minutes health is RED
+            if (old_timestamp + timedelta(minutes = 5)) < new_timestamp:
+                logging.info("UpdateHosts:  Health of %s is RED!")
+                hosts_list[i]["health"] = 'red'
+
+            # Over 1 minute health is YELLOW
+            elif (old_timestamp + timedelta(minutes = 1)) < new_timestamp:
+                logging.info("UpdateHosts:  Health of %s is YELLOW!")
+                hosts_list[i]["health"]  = 'yellow'
+
+            # else health is GREEN
             else:
-                logging.info("time_diff + 1m is MORE than new_timestamp")
+                logging.debug("UpdateHosts:  time_diff + 1m is MORE than new_timestamp. Health is GREEN!")
+                hosts_list[i]["health"] = 'green'
+
         except:
-            logging.info("HostID %s:  no data.", i)
+            logging.debug("UpdateHosts: except:  HostID %s:  no data.", i)
+            hosts_list[i]["health"] = 'no_data'
+
+
+def update_hosts(hostid, values):
+    logging.debug("UpdateHosts:  HostID:  %s,  values:  %s", hostid, values)
+
+    # update hosts list - before adding health or it gets wiped out
+    hosts_list[hostid] = values
+    logging.debug("UpdateHosts: hosts_list-hostid: %s", hosts_list[hostid])
+
+    logging.debug("UpdateHosts:  calling update_health next")
+    update_health()
+
+    logging.debug("Current content of hosts_list-hostid")
+    logging.debug(hosts_list[hostid])
+    host_data = (values["timestamp"], values["id"], values["hostname"], values["cpu_temp"])
+
+    logging.debug("calling set_color")
+    set_color(host_data)
 
 
 
 def set_color(host_data):
-    timestamp, hostId, hostname, temp = host_data
-#    host_temp = int(temp[0:5]) / 1000
-    host_temp = temp
-    if host_temp > 70:
-        host_temp = 70
-    id = int(hostId)
-    hue = (70 - host_temp) / 100
+    logging.debug("hot to set_color")
+    timestamp, hostid, hostname, temp = host_data
+
+    logging.debug("unpacked host_data")
+    logging.debug("temp = %s", temp)
+    # Necessary to stay 'red' when the temp tops our color range
+    if temp > 70:
+        temp = 70
+    id = int(hostid)
+    hue = (70 - temp) / 100
     r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
-    logging.debug(f"SetColor:  {hostId} {hostname} is {host_temp}C and {timestamp}")
+#    logging.debug(f"SetColor:  {hostid} {hostname} is {temp}C and {timestamp}")
+#    logging.debug("SetColor:  hosts_list for hostid %s", hostid, hosts_list[hostid])
     for y in range(4):
         uh.set_pixel(id, y, r, g, b)
     uh.show()
-
-# somewhere i will need a watchdog to know
-# def track_id(id):
-    id = id
-    logging.debug(f"Track id = {id}")
-
 
 
 client = mqtt.Client("mqtt_iamalive_teston-BB")
